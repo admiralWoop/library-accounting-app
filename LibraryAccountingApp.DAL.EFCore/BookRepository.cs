86febc730413d97129aaa415fac0a3f96348692d
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-07-05 00:04:43 +0700

    Added repositories

---
+++ LibraryAccountingApp.DAL.EFCore/BookRepository.cs
@@ -0,0 +1,51 @@
+﻿using LibraryAccountingApp.DAL.Contracts;
+using LibraryAccountingApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryAccountingApp.DAL.EFCore
+{
+    public class BookRepository : IRepository<Book>
+    {
+        private LibraryContext _context;
+
+        public BookRepository(LibraryContext context)
+        {
+            this._context = context;
+        }
+
+        public void Create(Book item)
+        {
+            _context.Books.Add(item);
+        }
+
+        public void Delete(long id)
+        {
+            Book book = _context.Books.Find(id);
+            if (book != null)
+                _context.Books.Remove(book);
+        }
+
+        public IEnumerable<Book> Find(Func<Book, bool> predicate)
+        {
+            return _context.Books.Where(predicate).ToList();
+        }
+
+        public Book Get(long id)
+        {
+            return _context.Books.Find(id);
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _context.Books;
+        }
+
+        public void Update(Book item)
+        {
+            _context.Books.Update(item);
+        }
+    }
+}
---
+++ LibraryAccountingApp.DAL.EFCore/GenreRepository.cs
@@ -0,0 +1,53 @@
+﻿using LibraryAccountingApp.DAL.Contracts;
+using LibraryAccountingApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryAccountingApp.DAL.EFCore
+{
+    public class GenreRepository : IRepository<Genre>
+    {
+        private LibraryContext _context;
+        
+        public GenreRepository(LibraryContext context)
+        {
+            this._context = context;
+        }
+
+        public void Create(Genre item)
+        {   
+            _context.Genres.Add(item);
+        }
+
+        public void Delete(long id)
+        {
+            var genre = _context.Genres.Find(id);
+            if(genre != null)
+            {
+                _context.Genres.Remove(genre);
+            }
+        }
+
+        public IEnumerable<Genre> Find(Func<Genre, bool> predicate)
+        {
+            return _context.Genres.Where(predicate).ToList();
+        }
+
+        public Genre Get(long id)
+        {
+            return _context.Genres.Find(id);
+        }
+
+        public IEnumerable<Genre> GetAll()
+        {
+            return _context.Genres;
+        }
+
+        public void Update(Genre item)
+        {
+            _context.Genres.Update(item);
+        }
+    }
+}
