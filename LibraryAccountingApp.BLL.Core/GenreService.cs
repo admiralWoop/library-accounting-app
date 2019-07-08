2c12fd861d66cc0631482810a363a931271f40b4
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-07-08 11:23:04 +0700

    Implemented GenreService

---
+++ LibraryAccountingApp.BLL.Core/GenreService.cs
@@ -0,0 +1,84 @@
+﻿using LibraryAccountingApp.DAL.Contracts;
+using LibraryAccountingApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryAccountingApp.BLL.Core
+{
+    public class GenreService : IDisposable
+    {
+        IUnitOfWork _uof;
+
+        public GenreService(IUnitOfWork uof)
+        {
+            _uof = uof;
+        }
+
+        public Genre GetById(long id) => _uof.Genres.Get(id);
+
+        public List<Genre> GetAll() => _uof.Genres.GetAll().ToList();
+
+        public void AddGenre(Genre genre)
+        {
+            if (_uof.Genres.Any(_ => _.Id == genre.Id)) //в БД есть жанр с таким Id
+            {
+                throw new ArgumentException($"Genre with Id={genre.Id} already exists");
+            }
+            else
+            {
+                _uof.Genres.Create(genre);
+                _uof.Save();
+            }
+        }
+
+        public void UpdateGenre(Genre newGenre)
+        {
+            if (_uof.Genres.Any(_ => _.Id == newGenre.Id)) //в БД есть жанр с таким Id
+            {
+                _uof.Genres.Update(newGenre);
+                _uof.Save();
+            }
+            else
+            {
+                throw new ArgumentException($"Book with Id={newGenre.Id} couldn't be found");
+            }
+        }
+
+        public void DeleteGenre(long id)
+        {
+            if (_uof.Genres.Any(_ => _.Id == id)) //в БД есть жанр с таким Id
+            {
+                _uof.Genres.Delete(id);
+                _uof.Save();
+            }
+            else
+            {
+                throw new ArgumentException($"Genre with Id={id} couldn't be found");
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _uof.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
