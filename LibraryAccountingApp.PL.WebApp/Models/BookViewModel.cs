16881dbaa3ebdcb09439d46532235eacf3b35689
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-07-12 03:22:26 +0700

    Moving from Razor Pages to MVC. Models.

---
+++ LibraryAccountingApp.PL.WebApp/Models/BookViewModel.cs
@@ -0,0 +1,31 @@
+﻿using LibraryAccountingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAccountingApp.PL.WebApp.Models
+{
+    public class BookViewModel
+    {
+
+        [DisplayName("Title")]
+        [Required(ErrorMessage = "Enter the title")]
+        public string Title { get; set; }
+
+        [DisplayName("Author name")]
+        [Required(ErrorMessage = "Enter the author name")]
+        public string AuthorName { get; set; }
+
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "Enter the description")]
+        public string Description { get; set; }
+
+        [DisplayName("Genre")]
+        [Required(ErrorMessage = "Enter the genre name")]
+        public string GenreName { get; set; }
+    }
+}
