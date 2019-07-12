82c5e1cc268a4e14ec8109af002ebddc6c0ead79
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-07-12 21:20:35 +0700

    Renamed genreservice to _genreService. Changed Index action. Added OpenBook action.

--- LibraryAccountingApp.PL.WebApp/Controllers/HomeController.cs
+++ LibraryAccountingApp.PL.WebApp/Controllers/HomeController.cs
@@ -14,32 +14,45 @@ namespace LibraryAccountingApp.PL.WebApp.Controllers
     public class HomeController : Controller
     {
         private BookService  _bookService;
-        private GenreService _genreservice;
+        private GenreService _genreService;
         private SignInManager<User> _singInManager;
 
         public HomeController(
-            BookService bookService, 
+            BookService bookService,
             GenreService genreService,
             SignInManager<User> singInManager)
         {
             _bookService = bookService;
-            _genreservice = genreService;
+            _genreService = genreService;
             _singInManager = singInManager;
         }
 
         public IActionResult Index()
         {
-            if(_singInManager.IsSignedIn(User))
+            if(true || _singInManager.IsSignedIn(User))
             {
                 var books = _bookService.GetAll();
                 var booksVM = books.Select(_ =>
                     new BookViewModel()
                     {
+                        Id = _.Id,
                         Title = _.Title,
                         AuthorName = _.AuthorName,
                         Description = _.Description,
                         GenreName = _.Genre.Name
                     });
+                var genres = _genreService.GetAll();
+                var genresVM = genres
+                    .Where(_ => !genres.Any(c => c.Subgenres.Contains(_)))
+                    .Select(_ =>
+                    new GenreViewModel()
+                    {
+                        Name = _.Name,
+                        Subgenres = _.Subgenres
+                    });
+
+                ViewBag.Books = booksVM;
+                ViewBag.Genres = genresVM;
                 return View("Index", booksVM);
             }
             else
@@ -47,6 +60,25 @@ namespace LibraryAccountingApp.PL.WebApp.Controllers
                 return RedirectToPage("/Account/Login",new { area = "Identity"});
             }
         }
+        
+        public IActionResult OpenBook(long? id)
+        {
+            if (!id.HasValue) return Error();
+            var book = _bookService.GetById(id.Value);
+            if (book == null) return Error();
+
+            var bookVM = new BookViewModel()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorName = book.AuthorName,
+                Description = book.Description,
+                GenreName = book.Genre?.Name
+            };
+
+            return View("BookView", bookVM);
+        }
+
 
         public IActionResult Privacy()
         {
