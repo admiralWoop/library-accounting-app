5ba2632727fa89aec790e0046282564637962170
Danila Pokhabov <danilapokhabov@protonmail.com>
2019-07-12 21:21:30 +0700

    Added GenreViewModel

---
+++ LibraryAccountingApp.PL.WebApp/Models/GenreViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LibraryAccountingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAccountingApp.PL.WebApp.Models
+{
+    public class GenreViewModel
+    {
+        [HiddenInput]
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Genre> Subgenres { get; set; }
+    }
+}
